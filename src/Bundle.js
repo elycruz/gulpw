b23655ea114fd1b77ce79347857a5e0cef2aa1a6
Ely De La Cruz <elycruz@elycruz.com>
2014-09-26 14:47:49 -0400

    Progress on initial scaffolding.

--- src/Bundle.js
+++ src/Bundle.js
@@ -1,3 +1,16 @@
 /**
  * Created by edelacruz on 9/19/2014.
  */
+
+require('sjljs');
+
+module.exports = (function () {
+
+    var Bundle = sjl.Extendable.extend(function Bundle () {},
+        {}, {
+            bundles_path: ""
+        });
+
+    return Bundle;
+
+}());
--- src/templates/gulpfile.js
+++ src/templates/gulpfile.js
@@ -1,3 +1,12 @@
 /**
  * Created by edelacruz on 9/19/2014.
  */
+var gulp        = require('gulp'),
+    concat      = require('gulp-concat'),
+    header      = require('gulp-header'),
+    mocha       = require('gulp-mocha'),
+    uglify      = require('gulp-uglify');
+
+module.exports = (function () {
+
+}());
