2f08845c5e8db433fc7a27f1387f95cf56f1392d
Ely De La Cruz <elycruz@elycruz.com>
2014-10-05 00:51:10 -0400

    Progress on sketching out initial idea.

--- src/Bundle.js
+++ src/Bundle.js
@@ -1,3 +1,12 @@
 /**
  * Created by edelacruz on 9/19/2014.
  */
+require('sjljs');
+
+modules.export = (function () {
+    return sjl.Extendable.extend(function Bundle () {
+
+    }, {
+
+    });
+})
--- src/GulpBundleWrangler.js
+++ src/GulpBundleWrangler.js
@@ -1,3 +1,13 @@
 /**
  * Created by Ely on 10/4/2014.
  */
+require('sjljs');
+
+modules.export = (function () {
+    return new sjl.Extendable.extend(function GulpBundleWrangler (config) {
+
+
+    }, {
+
+    });
+});
