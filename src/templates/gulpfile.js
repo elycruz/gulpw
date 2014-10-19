4e35852a6b771b295df9763158e3296e8a73f6c1
Ely De La Cruz <elycruz@elycruz.com>
2014-10-18 23:52:28 -0400

    Added some scaffolding for package.json file template and added uglify proxy task.

--- src/templates/gulpfile.js
+++ src/templates/gulpfile.js
@@ -6,3 +6,15 @@ var yaml = require('js-yaml'),
     wranglerConfig = yaml.safeLoad('gulp-bundle-wrangler.config.yaml'),
     wrangler = (new require('gulp-bundle-wrangler'))(wranglerConfig),
     gulp = wrangler.init(require('gulp'));
+
+gulp.task('gulpfile', function () {
+
+});
+
+gulp.task('bundle-wrangler-config', function () {
+
+});
+
+gulp.task('bundle-wrangler-bundle', function () {
+
+});
