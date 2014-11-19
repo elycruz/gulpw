dbdecbd375376fd73d0192e90d8acdaffb39811c
Ely De La Cruz <elycruz@elycruz.com>
2014-11-19 00:59:30 -0500

    Performed minor cleanup.

--- src/templates/gulpfile.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by edelacruz on 9/19/2014.
- */
-var yaml = require('js-yaml'),
-    Wrangler = require('./../Wrangler'),
-    wrangler = new Wrangler(),
-    gulp = wrangler.init( require('gulp') );
