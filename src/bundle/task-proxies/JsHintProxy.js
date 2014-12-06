4cb9969494212fd22c9d40dbccd0daa1766234a4
Ely De La Cruz <elycruz@elycruz.com>
2014-12-06 06:30:10 -0500

    Add the ability to ignore files in the jshint task.  Added gulp-fncallback module to be able to have callbacks in any point in gulp stream's pipeline.

--- src/bundle/task-proxies/JsHintProxy.js
+++ src/bundle/task-proxies/JsHintProxy.js
@@ -7,7 +7,10 @@ require('sjljs');
 var gulpif = require('gulp-if'),
     jshint = require('gulp-jshint'),
     duration = require('gulp-duration'),
-    TaskProxy = require('../TaskProxy');
+    TaskProxy = require('../TaskProxy'),
+    path = require('path'),
+    chalk = require('chalk'),
+    callback = require('gulp-fncallback');
 
 module.exports = TaskProxy.extend("JsHintProxy", {
 
@@ -21,17 +24,33 @@ module.exports = TaskProxy.extend("JsHintProxy", {
         // Task string separator
         var self = this,
             separator = wrangler.getTaskStrSeparator(),
+            taskName = 'jshint' + separator + bundle.options.name,
             jsHintConfig = wrangler.tasks.jshint.options,
-            useFailReporter = false;
+            useFailReporter = false,
+            filesToExclude = wrangler.tasks.jshint.ignoredFiles,
+            src;
 
         if (!self.isBundleValidForTask(bundle)) {
             return;
         }
 
-        gulp.task('jshint' + separator + bundle.options.name, function () {
-            gulp.src(bundle.options.files.js)
+        src = bundle.options.files.js;
+
+        if (filesToExclude && Array.isArray(filesToExclude)) {
+            src = src.filter(function (item) {
+                return filesToExclude.indexOf(path.basename(item)) === -1;
+            });
+        }
+
+        gulp.task(taskName, function () {
+            wrangler.log(chalk.cyan('Running "' + taskName + '"'), '--mandatory');
+            gulp.src(src)
+                .pipe(callback(function (file, enc, cb) {
+                    wrangler.log('Linting: ' + file.path, '--mandatory');
+                    return cb ? cb() : file;
+                }))
                 .pipe(jshint(jsHintConfig))
-                .pipe(duration("jshint \"" + bundle.options.name + "\" duration"))
+                .pipe(duration(chalk.cyan("jshint \"" + bundle.options.name + "\" duration")))
                 .pipe(jshint.reporter('jshint-stylish'))
                 .pipe(gulpif(useFailReporter, jshint.reporter('fail')));
         });
