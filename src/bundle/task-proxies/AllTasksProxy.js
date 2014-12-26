75d453e01623be18175247758d078c154751cc01
Ely De La Cruz <elycruz@elycruz.com>
2014-12-26 14:30:43 -0500

    Added preliminary 'all' task.

---
+++ src/bundle/task-proxies/AllTasksProxy.js
@@ -0,0 +1,53 @@
+/**
+ * Created by ElyDeLaCruz on 11/18/2014.
+ */
+'use strict';
+
+require('sjljs');
+
+// Import base task proxy to extend
+var TaskProxy = require('../TaskProxy'),
+    chalk = require('chalk');
+
+module.exports = TaskProxy.extend('AllTasksProxy', {
+
+    registerBundles: function (bundles, gulp, wrangler) {
+        var self = this,
+            tasks = [],
+            tasksList = wrangler.tasks.all.tasks;
+
+        bundles.forEach(function (bundle) {
+            if (!self.isBundleValidForTask(bundle)) {
+                return;
+            }
+            tasksList.forEach(function (task) {
+                if (task === 'build' && self.isBundleValidForBuildTask(bundle)) {
+                    tasks.push(task + ':' + bundle.options.alias);
+                }
+                else if (task === 'deploy' && self.isBundleValidForDeployTask(bundle)) {
+                    tasks.push(task + ':' + bundle.options.alias);
+                }
+                else if (task !== 'deploy' && task !== 'build') {
+                    tasks.push(task + ':' + bundle.options.alias);
+                }
+            });
+        });
+
+        gulp.task('all', function () {
+            self.launchTasks(tasks, gulp, wrangler);
+        });
+    },
+
+    isBundleValidForTask: function (bundle) {
+        return this.isBundleValidForBuildTask(bundle) || this.isBundleValidForDeployTask(bundle);
+    },
+
+    isBundleValidForBuildTask: function (bundle) {
+        return bundle && (bundle.has('files') || bundle.has('requirejs') || bundle.has('browserify'));
+    },
+
+    isBundleValidForDeployTask: function (bundle) {
+        return bundle && bundle.has('deploy.otherFiles');
+    }
+
+}); // end of export
