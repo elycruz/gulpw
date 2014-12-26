0ad5f4f82b494aea233f3019fe9ea559cf0c7e4b
Ely De La Cruz <elycruz@elycruz.com>
2014-12-26 13:02:39 -0500

    Added jshint task to project file.

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,20 @@
 /**
  * Created by Ely on 12/26/2014.
  */
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint'),
+    srcs = ['./src/**/*.js', './tests/**/*.js'];
+
+gulp.task('jshint', function () {
+    gulp.src(srcs)
+        .pipe(jshint())
+        .pipe(jshint.reporter('jshint-stylish'));
+});
+
+gulp.task('watch', function () {
+    gulp.watch(srcs, ['jshint'])
+        .pipe();
+});
+
+gulp.task('default', ['jshint', 'watch']);
