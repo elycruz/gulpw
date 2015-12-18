3d358d21d5bbff467a9bbdd549cb2a677f549353
Ely De La Cruz <elycruz@elycruz.com>
2015-12-17 21:48:29 -0500

    Progress on TaskManager.

--- src/TaskManager.js
+++ src/TaskManager.js
@@ -5,98 +5,25 @@
 
 'use strict';
 
-class TaskManager {
+let TaskManagerConfig = require('TaskManagerConfig');
+
+class TaskManager extends TaskManagerConfig {
 
     constructor(taskRunner, argv, env, config) {
-
-        var _bundleConfigsPath,
-            _bundleConfigFormats,
-            _localConfigPath,
-            _localConfigBackupPath,
-            _localHelpDocsPath,
-            _helpDocsPath,
-            _taskConfigs,
-            _taskAdapters,
+        var _taskAdapters,
             _taskNames,
-            _staticTaskConfigs,
+            _staticTaskAdapters,
             _staticTaskNames,
             _taskRunner,
             _env,
             _argv,
             _cwd,
-            _pwd,
-            _configPath;
+            _pwd;
 
         Object.defineProperties(this, {
-            _bundleConfigsPath: {
-                get: () => {
-                    return _bundleConfigsPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_bundlesConfigPath', value, String,
-                        'Only strings allowed for this property');
-                    _bundleConfigsPath = value;
-                }
-            },
-            _bundleConfigFormats: {
-                set: (value) => {
-                    var classOfValue = sjl.classOf(value);
-                    if (Array.isArray(value)) {
-                    }
-                },
-                get: () => {
-                    return _bundleConfigFormats;
-                }
-            },
-            _localConfigPath: {
-                get: () => {
-                    return _localConfigPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_localConfigPath', value, String,
-                        'Only strings allowed for this property');
-                    _bundleConfigsPath = value;
-                }
-            },
-            _localConfigBackupPath: {
-                get: () => {
-                    return _localConfigBackupPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_localConfigPath', value, String,
-                        'Only strings allowed for this property');
-                    _localConfigBackupPath = value;
-                }
-            },
-            _localHelpDocsPath: {
-                get: () => {
-                    return _localHelpDocsPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_localHelpDocsPath', value, String,
-                        'Only strings allowed for this property');
-                    _localHelpDocsPath = value;
-                }
-            },
-            _helpDocsPath: {
-                get: () => {
-                    return _helpDocsPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_helpDocsPath', value, String,
-                        'Only strings allowed for this property');
-                    _helpDocsPath = value;
-                }
-            },
-            _taskConfigs: null,
             _taskAdapters: null,
             _taskNames: null,
-            _staticTaskConfigs: null,
+            _staticTaskAdapters: null,
             _staticTaskNames: null,
             _taskRunner: null,
             _env: env,
@@ -122,26 +49,17 @@ class TaskManager {
                         'Only strings allowed for this property');
                     _pwd = value;
                 }
-            }, //env.pwd,
-            _configPath: {
-                get: () => {
-                    return _configPath;
-                },
-                set: (value) => {
-                    sjl.throwTypeErrorIfNotOfType(
-                        TaskManager.name, '_configPath', value, String,
-                        'Only strings allowed for this property');
-                    _configPath = value;
-                }
-            } //env.configPath
-
+            } //env.pwd,
         });
+
+        super(config);
 
         sjl.extend(true, this, {
             _cwd: env.configBase,
             _pwd: env.pwd,
             _configPath: env.configPath
         });
+
     }
 
     getTaskAdapter(taskName) {
--- src/TaskManagerConfig.js
+++ src/TaskManagerConfig.js
@@ -9,6 +9,119 @@
 
     class TaskManagerConfig extends Config {
         constructor(...options) {
+            var _bundleConfigsPath,
+                _bundleConfigFormats,
+                _localConfigPath,
+                _localConfigBackupPath,
+                _localHelpDocsPath,
+                _helpDocsPath,
+                _taskConfigs,
+                _staticTaskConfigs,
+                _configPath;
+
+            Object.defineProperties(this, {
+                _bundleConfigsPath: {
+                    get: () => {
+                        return _bundleConfigsPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_bundlesConfigPath', value, String,
+                            'Only strings allowed for this property');
+                        _bundleConfigsPath = value;
+                    }
+                },
+                _bundleConfigFormats: {
+                    set: (value) => {
+                        var classOfValue = sjl.classOf(value);
+                        if (Array.isArray(value)) {
+                        }
+                    },
+                    get: () => {
+                        return _bundleConfigFormats;
+                    }
+                },
+                _localConfigPath: {
+                    get: () => {
+                        return _localConfigPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_localConfigPath', value, String,
+                            'Only strings allowed for this property');
+                        _bundleConfigsPath = value;
+                    }
+                },
+                _localConfigBackupPath: {
+                    get: () => {
+                        return _localConfigBackupPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_localConfigPath', value, String,
+                            'Only strings allowed for this property');
+                        _localConfigBackupPath = value;
+                    }
+                },
+                _localHelpDocsPath: {
+                    get: () => {
+                        return _localHelpDocsPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_localHelpDocsPath', value, String,
+                            'Only strings allowed for this property');
+                        _localHelpDocsPath = value;
+                    }
+                },
+                _helpDocsPath: {
+                    get: () => {
+                        return _helpDocsPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_helpDocsPath', value, String,
+                            'Only strings allowed for this property');
+                        _helpDocsPath = value;
+                    }
+                },
+                _taskConfigs: {
+                    get: () => {
+                        return _taskConfigs;
+                    },
+                    set: (value) => {
+                        var classOfValue = sjl.classOf(value);
+                        if (classOfValue === 'Object') {
+                            _taskConfigs = new sjl.package.stdlib.SjlMap();
+                            // @todo finish this
+                        }
+                        else if (classOfValue === 'SjlMap') {
+                            _taskConfigs = value;
+                        }
+                        else {
+                            throw new Error(TaskManagerConfig)
+                        }
+                    }
+                },
+                _staticTaskConfigs: null,
+                _configPath: {
+                    get: () => {
+                        return _configPath;
+                    },
+                    set: (value) => {
+                        sjl.throwTypeErrorIfNotOfType(
+                            TaskManager.name, '_configPath', value, String,
+                            'Only strings allowed for this property');
+                        _configPath = value;
+                    }
+                } //env.configPath
+            });
+
+            sjl.extend(true, this, {
+                _cwd: env.configBase,
+                _pwd: env.pwd,
+                _configPath: env.configPath
+            });
             super({
                 bundleConfigsPath: '',
                 bundleConfigFormats: [],
